ace434ee9bd8555a29b555bc50f16ca3260806e4
ankishiv65 <ankishiv65@gmail.com>
2020-09-10 02:38:47 +0530

    second commit

--- src/test/java/Sachin/Tendulkar/india.java
+++ src/test/java/Sachin/Tendulkar/india.java
@@ -5,5 +5,11 @@ public class india {
 	public void run() {
 		System.out.println("India");
 	}
+	public void swim() {
+		System.out.println("swim");
+	}
+	public void walk() {
+		System.out.println("walk");
+	}
 
 }
